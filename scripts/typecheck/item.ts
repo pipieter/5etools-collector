cc8294ddcac26f981986396317881af7a3160a34
Pim Pieters <pim@pieters.tv>
2026-08-25 15:57:58 +0200

    Add item fluff checking

--- scripts/typecheck/item.ts
+++ scripts/typecheck/item.ts
@@ -3,10 +3,14 @@ import { check } from '../check';
 
 import BaseTI from '../../types-check/base-ti';
 import EntryTI from '../../types-check/entry-ti';
+import FluffTI from '../../types-check/fluff-ti';
 import ItemTI from '../../types-check/item-ti';
 
 import official from '../../data/official/items.json';
+import officialFluff from '../../data/official/item-fluffs.json';
 
 const { Item } = createCheckers(BaseTI, EntryTI, ItemTI);
+const { Fluff } = createCheckers(BaseTI, EntryTI, FluffTI);
 
 check('item', Item, official);
+check('item fluff', Fluff, officialFluff);
