a810b4474a4b1a499b3b03419cb9237dc12b025c
Pim Pieters <pim@pieters.tv>
2026-09-05 16:37:48 +0200

    Remove _copy from base

--- types/internal/base.ts
+++ types/internal/base.ts
@@ -51,7 +51,6 @@ export interface Base extends Foundry, TokenArt, HasFluff {
   additionalSources?: Source[];
   edition?: string;
   _versions?: any[]; // TODO
-  _copy?: any; // TODO
 }
 
 export type Source =
